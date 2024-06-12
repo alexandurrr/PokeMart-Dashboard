071e549d7ab6f8c0e698a9a2f54c6b2494b94201
Alexander Gyatso <alexandeg@uia.no>
2024-06-12 11:36:05 +0200

    added file structure

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import './App.css'
 
 const App = () => {
+    const activeMenu = false;
   return (
     <div>
         <BrowserRouter>
@@ -16,11 +17,58 @@ const App = () => {
                     <button type="button"
                     className="text-3xl p-3 
                     hover:drop-shadow-xl 
-                    hover:bg-light-gray">
+                    hover:bg-light-gray text-white"
+                    style={{background: 'blue', 
+                    borderRadius: '50%'}}>
                         <FiSettings/>
                     </button>
 
                 </TooltipComponent>
+            </div>
+            {activeMenu ? (
+                <div className="w-72 fixed sidebar
+                dark:bg-secondary-dark-bg bg-white">
+                    Sidebar
+                </div>
+            ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">
+                Sidebar w-0
+            </div>
+                )}
+            <div className={
+                `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-7' : 'flex-2'}`
+            }>
+                 <div className="fixed md:static 
+                 bg-main-bg dark:bg-main-dark-bg 
+                 navbar w-full">
+                    Navbar
+                 </div>
+            </div>
+            <div>
+                <Routes>
+                    {/* Dashboard route */}
+                    <Route path="/" element="ECommerce"/>
+                    <Route path="/ecommerce" element="ECommerce"/>
+                    {/* Pages */}
+                    <Route path="/orders" element="Orders"/>
+                    <Route path="/employees" element="Employees"/>
+                    <Route path="/customers" element="Customers"/>
+                    {/* Apps */}
+                    <Route path="/kanban" element="Kanban"/>
+                    <Route path="/editor" element="Editor"/>
+                    <Route path="/calendar" element="Calendar"/>
+                    <Route path="/color-picker" element="ColorPicker"/>
+                    
+                    {/* Charts */}
+                    <Route path="/line" element="Line"/>
+                    <Route path="/area" element="Area"/>
+                    <Route path="/bar" element="Bar"/>
+                    <Route path="/pie" element="Pie"/>
+                    <Route path="/financial" element="Financial"/>
+                    <Route path="/color-mapping" element="ColorMapping"/>
+                    <Route path="/pyramid" element="Pyramid"/>
+                    <Route path="/stacked" element="Stacked"/>
+                </Routes>
             </div>
         </div>
         </BrowserRouter>
