a6364e28e50aebc80dfe81966adbae9279394a1d
Alexander Gyatso <alexandeg@uia.no>
2024-06-17 14:13:30 +0200

    Added several pages, fixed sidebar bug, added datatables

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ const App = () => {
           )}
           <div
             className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
-              activeMenu ? "md:ml-7" : "flex-2"
+              activeMenu ? "md:ml-72" : "flex-2"
             }`}
           >
             <div
